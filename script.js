45ae9cf1909b9dae30ffa211621fa662b8783af9
Michelle Jordan <tmsmith1150@live.com>
2020-05-14 18:13:26 -0400

    update 5.14

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var myTimer = document.getElementById("timer")
 
+var myQuestion = document.getElementById("question")
+var userAnswer = document.querySelectorAll(".buttons")
 
 
 
+
+myQuestion.textContent = "Commonly used data types DO NOT include:";
+
+// $("li").text("strings")
 
