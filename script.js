bf8b8c1c668879dc19bd26d54ec36075109b7f2f
Michelle Jordan <tmsmith1150@live.com>
2020-05-16 17:20:09 -0400

    updating code-quiz

--- script.js
+++ script.js
@@ -1,88 +1,139 @@
 
 
-var timeLeft = 30;
-var myTimer = $("timer");
-var startQuizBtn = $("nav-item nav-link");
-
-    
-var timerId = setInterval(countdown, 1000);
-
-    function countdown() {
-      if (timeLeft == 0) {
-        clearTimeout(timerId);
-        // doSomething();
-      } else {
-        myTimer = ("Time: ", + timeLeft--);
-      }
-    }
 
 
-
-
-var myQuestion = $("question");
-var userAnswer = $(".buttons");
-var myScore = $("score");
-myScore = 0;
-
-var userA = $("button-1");
-var userB = $("button-2");
-var userC = $("button-3");
-var userD = $("button-4");
+// var startQuizBtn = $("nav-item nav-link");
+// $("nav-item nav-link").on("click", function(e) {
+// var timeleft = 30;
+// var downloadTimer = setInterval(function(){
+// timeleft--;
+// document.getElementById("countdowntimer").textContent = timeleft;
+// if(timeleft <= 0)
+//     clearInterval(downloadTimer);
+// },1000);
+// })
 
 
 
 
 
 
+var questionIndex = 0;
 
 
- $("h2").text("Commonly used data types DO NOT include:");
 
- $("#button-1").text("strings");
- $("#button-2").text("booleans");
- $("#button-3").text("alerts");
- $("#button-4").text("numbers");
+var question = [
+  "Commonly used data types DO NOT include:",
+  "The condition of an if/else statement is enclosed within ___.",
+  "Arrays in Javascript can be used to store ___.",
+  "String values must be enclosed within ___ when being assigned to variables.",
+  "A very useful tool used during development and debugging for printing content to the debugger is:"
+]
 
+var q1Answers = [
+ "strings",
+ "booleans",
+ "alerts",
+ "numbers"
+]
 
-$(".buttons").on("click", function() {
-    $.each(userAnswer, function(i, item) {
-        $("h2").text("The condition of an if/else statement is enclosed with:");
-        $("#button-1").text("quotes");
-        $("#button-2").text("curly brackets");
-        $("#button-3").text("parenthesis");
-        $("#button-4").text("square brackets");
-    })
-    if ("#button-3" === true) {
-    alert("score: " + myScore++);
+var q2Answers = [
+ "quotes",
+ "curly brackets",
+ "parenthesis",
+ "square brackets"
+]
 
-    
+var q3Answers = [
+ "numbers and strings",
+ "other arrays",
+ "booleans",
+ "all of the above"
+]
 
-    }
-})
-
-$(".buttons").on("click", function() {
-    $.each(userAnswer, function(i, item) {
-        $("h2").text("Arrays in Javascript can be used to store:");
-        $("#button-1").text("numbers and strings");
-        $("#button-2").text("other arrays");
-        $("#button-3").text("booleans");
-        $("#button-4").text("all of the above");
-    })
-    if ("#button-3" === true) {
-    alert("score: " + myScore++);
+var q4Answers = [
+"commas",
+"curly brackets",
+"quotes",
+"parenthesis"
+]
 
     
 
-    }
-})
+
+GetQuestion(questionIndex);
+
+GetQuestion(index)
+
+$.each(questionIndex, function(i, item) {
+ $("h2").text(question[index]);
+
+ if(index == 1)
+ {
+  $("#button-1").text(q1Answers[0]);
+  $("#button-2").text(q1Answers[1]);
+  $("#button-3").text(q1Answers[2]);
+  $("#button-4").text(q1Answers[3]);
+ }
+ else if(index == 2)
+ {
+   
+  $("#button-1").text(q2Answers[0]);
+  $("#button-2").text(q2Answers[1]);
+  $("#button-3").text(q2Answers[2]);
+  $("#button-4").text(q2Answers[3]);
+ }
+ else if(index == 3)
+ {
+   
+  $("#button-1").text(q3Answers[0]);
+  $("#button-2").text(q3Answers[1]);
+  $("#button-3").text(q3Answers[2]);
+  $("#button-4").text(q3Answers[3]);
+ }
+ else if(index == 4)
+ {
+   
+  $("#button-1").text(q4Answers[0]);
+  $("#button-2").text(q4Answers[1]);
+  $("#button-3").text(q4Answers[2]);
+  $("#button-4").text(q4Answers[3]);
+ }
+ else if(index == 5)
+ {
+   
+  $("#button-1").text(q5Answers[0]);
+  $("#button-2").text(q5Answers[1]);
+  $("#button-3").text(q5Answers[2]);
+  $("#button-4").text(q5Answers[3]);
+ }
+
+ questionIndex++;
+
+getQuestion()
 
 
 
 
 
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("done");
+var span = document.getElementsByClassName("close")[0];
 
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
@@ -131,3 +182,79 @@ $(".buttons").on("click", function() {
 //     fifth: "console.log",
     
 // }
+
+
+
+
+
+
+// var timerId = setInterval(countdown, 1000);
+
+//     function countdown() {
+//       if (timeLeft == 0) {
+//         clearTimeout(timerId);
+//         // doSomething();
+//       } else {
+//         myTimer = ("Time: ", + timeLeft--);
+//       }
+//     }
+
+
+
+
+// var myQuestion = $("question");
+// var userAnswer = $(".buttons");
+// var myScore = $("score");
+// myScore = 0;
+
+// var userA = $("button-1");
+// var userB = $("button-2");
+// var userC = $("button-3");
+// var userD = $("button-4");
+
+
+
+
+
+
+
+
+//  $("h2").text("Commonly used data types DO NOT include:");
+
+//  $("#button-1").text("strings");
+//  $("#button-2").text("booleans");
+//  $("#button-3").text("alerts");
+//  $("#button-4").text("numbers");
+
+
+// $(".buttons").on("click", function() {
+//     $.each(userAnswer, function(i, item) {
+//         $("h2").text("The condition of an if/else statement is enclosed with:");
+//         $("#button-1").text("quotes");
+//         $("#button-2").text("curly brackets");
+//         $("#button-3").text("parenthesis");
+//         $("#button-4").text("square brackets");
+//     })
+//     if ("#button-3" === true) {
+//     alert("score: " + myScore++);
+
+    
+
+//     }
+// })
+
+// $(".buttons").on("click", function() {
+//     $.each(userAnswer, function(i, item) {
+//         $("h2").text("Arrays in Javascript can be used to store:");
+//         $("#button-1").text("numbers and strings");
+//         $("#button-2").text("other arrays");
+//         $("#button-3").text("booleans");
+//         $("#button-4").text("all of the above");
+//     })
+//     if ("#button-3" === true) {
+//     alert("score: " + myScore++);
+
+    
+
+//     }
+// })
